7e91caca6d6ac2e2b2eebeaa88645b7302d84fc7
tyriquezim <zimbizi.tyrique@gmail.com>
2025-01-27 15:01:06 +0800

    Modified the initialisePlayers function to insert the observers into the players during initialisation

--- app/src/main/java/com/android/personal/pentago/PentagoApplication.kt
+++ app/src/main/java/com/android/personal/pentago/PentagoApplication.kt
@@ -4,6 +4,13 @@ import android.app.Application
 import androidx.collection.emptyLongSet
 import com.android.personal.pentago.model.Marble
 import com.android.personal.pentago.model.PlayerProfile
+import com.android.personal.pentago.observers.AchievementObserver
+import com.android.personal.pentago.observers.DrawAchievementObserver
+import com.android.personal.pentago.observers.GamesPlayedAchievementObserver
+import com.android.personal.pentago.observers.LoseAchievementObserver
+import com.android.personal.pentago.observers.MoveAchievementObserver
+import com.android.personal.pentago.observers.WinAchievementObserver
+import com.android.personal.pentago.observers.WinPercentageAchievementObserver
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -31,6 +38,37 @@ class PentagoApplication: Application()
                 val player1 = PlayerProfile(getString(R.string.player_1_default_username), PlayerProfile.DEFAULT_PP, Marble.RED_MARBLE)
                 val player2 = PlayerProfile(getString(R.string.player_2_default_username), PlayerProfile.DEFAULT_PP, Marble.BLUE_MARBLE)
                 val aiPlayer = PlayerProfile(getString(R.string.ai_player_name), PlayerProfile.AI_ROBOT_PP, Marble.METALLIC_MARBLE)
+
+                val playerList = listOf(player1, player2, aiPlayer)
+
+                lateinit var achievementObserver: AchievementObserver
+
+                for(player in playerList)
+                {
+                    //Add Draw Observer
+                    achievementObserver = DrawAchievementObserver()
+                    player.addAchievementObserver(achievementObserver)
+
+                    //Add Games Played Observer
+                    achievementObserver = GamesPlayedAchievementObserver()
+                    player.addAchievementObserver(achievementObserver)
+
+                    //Add Lose Observer
+                    achievementObserver = LoseAchievementObserver()
+                    player.addAchievementObserver(achievementObserver)
+
+                    //Add Moves Played Observer
+                    achievementObserver = MoveAchievementObserver()
+                    player.addAchievementObserver(achievementObserver)
+
+                    //Add Win Achievement Observer
+                    achievementObserver = WinAchievementObserver()
+                    player.addAchievementObserver(achievementObserver)
+
+                    //Add Win Percentage Observer
+                    achievementObserver = WinPercentageAchievementObserver()
+                    player.addAchievementObserver(achievementObserver)
+                }
 
                 PentagoRepository.get().insertPlayerProfile(player1)
                 PentagoRepository.get().insertPlayerProfile(player2)
